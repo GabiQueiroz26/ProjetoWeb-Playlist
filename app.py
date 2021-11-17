5a0f36d96d3983c55c6de9d4f25acd3bbf3abcc0
Gabi Queiroz <freiras98@gmail.com>
2021-11-17 14:20:08 +0000

    Lista de Musicas

--- app.py
+++ app.py
@@ -1,13 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return render_template('index.html', nome = 'fulano')
+@app.route('/login')
+def home():
+    return render_template('login.html')
 
-@app.route('/bye')
-def bye():
-    return render_template('tchau.html')
+
+lista_musicas = [
+    {"musica": "Wish you were here", "artista": "Pink Floyd", "genero": "Rock"},
+    {"musica": "Feel Good Inc.", "artista": "Gorillaz", "genero": "Rap"},
+    {"musica": "Born to die", "artista": "Lana Del Rey", "genero": "Indie"},
+    {"musica": "Como os nossos pais", "artista": "Elis Regina", "genero": "MPB"},
+    {"musica": "Rindo à toa", "artista": "Falamansa", "genero": "Forró"},
+]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', lista=lista_musicas)
+
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+
+@app.route('/save', methods=['POST'])  # <form action="/save" method="POST">
+def save():
+    new_music = request.form['music']   # <input name="new_music"/>
+    new_artist = request.form['artist']       # <input name="new_artist"/>
+    id_gender = request.form['gender']       # <input name="new_gender"/>
+    new = { "musica": new_music, "artista": new_artist, "genero": id_gender}
+    lista_musicas.append(new)
+    
+    return redirect('https://5000-scarlet-tortoise-9tqc3dof.ws-us18.gitpod.io/')
+
+
+@app.route('/delete')
+def delete():
+    return render_template('delete.html')
+   
+
+@app.route('/remove', methods=['POST'])  # <form action="/save" method="POST">
+def remove():
+    new_music = request.form['music']   # <input name="new_music"/>
+    new_artist = request.form['artist']       # <input name="new_artist"/>
+    id_gender = request.form['gender']       # <input name="new_gender"/>
+    deletar = { "musica": new_music, "artista": new_artist, "genero": id_gender}
+    lista_musicas.append(deletar.clear())
+           
+    return redirect('https://5000-scarlet-tortoise-9tqc3dof.ws-us18.gitpod.io/')
 
 app.run(debug=True)
